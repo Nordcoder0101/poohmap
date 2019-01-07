c9ec359a1e985eaea94c491451b29ace232e02cd
Greg <greg@Gregs-MacBook-Air.local>
2019-01-07 14:42:47 -0800

    greetings added

--- poohBear.js
+++ poohBear.js
@@ -1,14 +1,59 @@
-let tigger = {character: "Tigger"}
-let pooh = {character: "Winnie The Pooh"}
-let piglet = { character: "Piglet"}
-let bees = { character: "Bees" }
-let owl = { character: "Owl" }
-let christopherRobbin = { character: "Christopher Robbin" }
-let rabbit = { character: "Rabbit" }
-let gopher = { character: "Gopher"}
-let kango = { character: "Kango" }
-let eeyore = { character: "Eeyore" }
-let heffalumps = {character: "Heffalumps" }
+let tigger = {
+  character: "Tigger",
+  greet: function(){
+    console.log("The wonderful thing about Tiggers is Tiggers are wonderful things!")
+  }  
+}
+let pooh = {character: "Winnie The Pooh",
+  greet: function () {
+    console.log("Oh brother")
+  }  
+}
+let piglet = { character: "Piglet",
+  greet: function () {
+    console.log("Oh d-d-d-d-dear!  I wasn't expecting company!")
+  }  
+}
+let bees = { character: "Bees",
+  greet: function () {
+    console.log("Bzzzzzz")
+  }  
+ }
+let owl = { character: "Owl" ,
+  greet: function () {
+    console.log("hoot, hoot")
+  }  
+}
+let christopherRobbin = { character: "Christopher Robbin",
+  greet: function () {
+    console.log("Well, hello!")
+  }  
+ }
+let rabbit = { character: "Rabbit",
+  greet: function () {
+    console.log("Hoppity Hop!")
+  }  
+ }
+let gopher = { character: "Gopher",
+  greet: function () {
+    console.log("Front or back 9")
+  }  
+}
+let kango = { character: "Kango",
+  greet: function () {
+    console.log("Boingy boing")
+  }  
+ }
+let eeyore = { character: "Eeyore",
+  greet: function () {
+    console.log("Eff my life!")
+  }  
+ }
+let heffalumps = {character: "Heffalumps",
+  greet: function () {
+    console.log("greets")
+  }  
+ }
 let player = {
   location: tigger
 }
@@ -16,21 +61,25 @@ let player = {
 function north(){
   player.location = player.location.north
   console.log(`You are now at ${player.location.character}`)
+  player.location.greet();
 }
 
 function south() {
   player.location = player.location.south
   console.log(`You are now at ${player.location.character}`)
+  player.location.greet();
 }
 
 function east() {
   player.location = player.location.east
   console.log(`You are now at ${player.location.character}`)
+  player.location.greet();
 }
 
 function west() {
   player.location = player.location.west
   console.log(`You are now at ${player.location.character}`)
+  player.location.greet();
 }
 
 
